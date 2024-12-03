adb21028a407c4496bf5a2bdc8fcd98209281ddd
kent.liu.philippines@gmail.com <liu947466968>
2024-12-03 17:49:49 +0800

    1

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints import nginx, ssl, deploy ,sites
 from app.core.init_db import initialize_database
 from app.core.config import settings
+from app.utils.shell import run_command
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -39,4 +41,21 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
+    # 定时执行更新代码 定时配置 2秒执行一次
+    async def auto_update():
+        while True:
+            await asyncio.sleep(2)
+            await auto_update()
     uvicorn.run(app, host="0.0.0.0", port=9999) 
+
+ 
+
+
+
+#这里需要一个自动更新代码的脚本
+async def auto_update():
+    # 1. 拉取最新代码 在当前项目的根目录执行 git pull
+    # 获取当前项目的根目录  
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    await run_command(f"cd {current_dir} && git pull")
+    pass
